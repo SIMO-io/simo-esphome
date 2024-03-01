36255e88655660fbb537c348b45dac7d03f646a4
simanas <simanas.venckauskas@gmail.com>
2024-03-01 15:36:03 +0200

    Moving to Django 4.2

--- src/simo_esphome/auto_urls.py
+++ src/simo_esphome/auto_urls.py
@@ -1,26 +1,25 @@
-from django.urls import path
-from django.conf.urls import url
+from django.urls import re_path, path
 
 from .autocomplete_views import BoardSelectAutocomplete, PinsSelectAutocomplete
 from .views import firmware_status_view, rebuild_firmware_view, wifi_update_view
 
 
 urlpatterns = [
-    path(
+    re_path(
         'autocomplete-espboard',
         BoardSelectAutocomplete.as_view(), name='autocomplete-espboard'
     ),
-    path(
+    re_path(
         'autocomplete-espboard-pins',
         PinsSelectAutocomplete.as_view(), name='autocomplete-espboard-pins'
     ),
-    url(
+    re_path(
         r'^firmware-status/(?P<id>[0-9]+)/$', firmware_status_view,
     ),
-    url(
+    re_path(
         r'^rebuild-firmware/(?P<id>[0-9]+)/$', rebuild_firmware_view,
     ),
-    url(
+    re_path(
         r'^wifi-update/(?P<id>[0-9]+)/$', wifi_update_view,
     )
 ]
